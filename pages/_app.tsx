4dcc9106d1aa9d417338830e16c9242a5f444b70
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-06-23 00:33:54 +0200

    login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import RootLayout from "../components/root-layout";
-
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { supabase } from '../lib/supabaseClient'
+ 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
     <>
+     <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}>
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
       <div id="root" />
+    </SessionContextProvider>
     </>
   );
 }
