f5e736f3853579eeaa907469f00d77af25cb9703
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-06-27 18:43:14 +0200

    fixes

--- pages/blog/[slug]/index.jsx
+++ pages/blog/[slug]/index.jsx
@@ -1,38 +1,37 @@
-import { useRouter } from "next/router";
-import styles from "./blog-post.module.css";
-import Comments from "./partials/comments";
-import AddComment from "./partials/add-comment";
-import Button from "@components/button";
-import Heading from "@components/heading";
-import BlogImageBanner from "@components/blog-image-banner";
-import useSWRMutation from "swr/mutation";
-import useSWR from "swr";
-import { removePost, getPost, postsCacheKey } from "../../../api-routes/posts";
-import { useUser } from "@supabase/auth-helpers-react";
+import AddComment from './partials/add-comment';
+import BlogImageBanner from '@components/blog-image-banner';
+import Button from '@components/button';
+import Comments from './partials/comments';
+import Heading from '@components/heading';
+import styles from './blog-post.module.css';
+import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
+import { isAuthorLogedIn } from '@/utils/isAuthorLogedIn';
+import { manipulateDate } from '@/utils/manipulateDate';
+import { postCacheKey, getPost, removePost } from '@/api-routes/posts';
+import { useRouter } from 'next/router';
 
 export default function BlogPost() {
   const router = useRouter();
-  const user = useUser();
   const { slug } = router.query;
 
-  const { data: { data = [] } = {}, error } = useSWR(
-    slug ? `${postsCacheKey}${slug}` : null,
-    () => getPost({ slug })
-  );
+  const {
+    data: { data = [] } = {},
+    error,
+    status,
+  } = useSWR(slug ? `${postCacheKey}${slug}` : null, () => getPost({ slug }));
 
-  const { trigger: deleteTrigger, isMutating } = useSWRMutation(postsCacheKey, removePost, {
-    onError: (error) => {
-      console.log(error);
-    },
-  });
+  const { trigger: deleteTrigger } = useSWRMutation(postCacheKey, removePost);
 
-  const handleDeletePost = async () => {
-    const postId = data?.id;
-    const { status, error } = await deleteTrigger(postId);
+  const handleDeletePost = async (id) => {
+    const { error, status } = await deleteTrigger(id);
 
-    if (!error) {
-      router.push(`/blog`);
+    if (error || status !== 204) {
+      console.log({ status, error });
+      return;
     }
+
+    router.push('/blog');
   };
 
   const handleEditPost = () => {
@@ -41,36 +40,27 @@ export default function BlogPost() {
 
   return (
     <>
-      {data && (
-        <section className={styles.container}>
-          {data.title && <Heading>{data.title}</Heading>}
-          {data.image && (
-            <BlogImageBanner src={data.image} alt={data.title} />
-          )}
-          <div className={styles.dateContainer}>
-            {data.created_at && (
-              <time className={styles.date}>{data.created_at}</time>
-            )}
-            <div className={styles.border} />
+      <section className={styles.container}>
+        <Heading>{data.title}</Heading>
+        {data?.image && <BlogImageBanner src={data.image} alt={data.title} />}
+        <div className={styles.dateContainer}>
+          <time className={styles.date}>{manipulateDate(data.created_at)}</time>
+          <div className={styles.border} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: data.body }} />
+        <span className={styles.author}>Author: {data.author}</span>
+
+        {isAuthorLogedIn({ postAuthor: data.user_id }) && (
+          <div className={styles.buttonContainer}>
+            <Button onClick={() => handleDeletePost(data.id)}>Delete</Button>
+            <Button onClick={handleEditPost}>Edit</Button>
           </div>
-          <div dangerouslySetInnerHTML={{ __html: data.body }} />
-          <span className={styles.author}>
-            Author: {data?.Users?.alias ?? "Author"}
-          </span>
+        )}
+      </section>
 
-          {user && (
-            <div className="flex text-darkColor mt-2">
-              <Button onClick={() => handleDeletePost(data.id)} className="mr-2">
-                Delete
-              </Button>
-              <Button onClick={handleEditPost}>Edit</Button>
-            </div>
-          )}
-        </section>
-      )}
+      <Comments postId={data.id} postAuthorId={data.user_id} />
 
-      {user && <AddComment postId={data?.id} />}
-      <Comments postId={data?.id} />
+      <AddComment postId={data.id} />
     </>
   );
 }
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,20 +1,20 @@
-import Link from "next/link";
-import styles from "./blog.module.css";
-import Heading from "@components/heading";
-import { getPosts, postsCacheKey } from "../../api-routes/posts";
-import useSWR from 'swr'
-import { useUser } from "@supabase/auth-helpers-react";
+import Heading from '@components/heading';
+import Link from 'next/link';
+import styles from './blog.module.css';
+import useSWR from 'swr';
+import { manipulateDate } from '@/utils/manipulateDate';
+import { postCacheKey, getPosts } from '@/api-routes/posts';
+import { useUser } from '@supabase/auth-helpers-react';
 
-export default function Blog() {  
-  const user = useUser()
+export default function Blog() {
+  const { data: { data = [] } = {} } = useSWR(postCacheKey, getPosts);
 
-  const {
-    data: {data = []} = {}, 
-    error
-  } = useSWR(postsCacheKey, getPosts)
+
+  const user = useUser();
+  console.log(user);
 
   return (
-    <section className="w-full">
+    <section>
       <Heading>Blog</Heading>
       {data?.map((post) => (
         <Link
@@ -22,9 +22,11 @@ export default function Blog() {
           className={styles.link}
           href={`/blog/${post.slug}`}
         >
-          <div className="w-full flex flex-col">
-            <p className="text-xl">{post.title}</p>
-            <time className='text-sm text-accentPurple'>{post.created_at.slice(0, 16)}</time>
+          <div className='w-full flex flex-col'>
+            <p>{post.title}</p>
+            <time className={styles.date}>
+              {manipulateDate(post.created_at)}
+            </time>
           </div>
         </Link>
       ))}
